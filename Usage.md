f8f5f8d2be92546447421f629f758dcfd1b7132f
pwu <yuanpeng.wu@gmail.com>
2018-03-05 22:36:55 -0800

    Initialize Finance Free project with scrapy parser
    This is a finance free project that can be used to do fundamental
    analysis. You can easily use the tool and script I provide to scrapy the
    important information from Yahoo finance pages.
    
    I will implement 5 important elements analysis and generate excel graphs
    for each stock later.

---
+++ Usage.md
@@ -0,0 +1,9 @@
+finance hypothesis
+http://www.euroinvestor.com/ei-news/2012/02/12/stock-school-5-important-elements-in-fundamental-analysis/15694
+
+software install
+python3
+scrapy
+https://docs.scrapy.org/en/latest/intro/install.html#intro-install				
+
+run program
---
+++ scrapyDB/tutorial/spiders/finance_spider.py
@@ -0,0 +1,52 @@
+import scrapy
+import re
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "financeQuotes"
+
+    def start_requests(self):
+        def generateSourceLink():
+            reList = []
+            txt_file = open('stocklist.txt', "r")
+            lines = txt_file.readlines()
+            for line in lines:
+                line = line.strip()
+                tmpStr = "https://finance.yahoo.com/quote/%s/financials?p=%s"% (line, line)
+                reList.append(str(tmpStr))
+            return reList
+
+        urls = generateSourceLink()
+        '''
+        urls = [
+            "https://finance.yahoo.com/quote/GE/financials?p=GE",
+            "https://finance.yahoo.com/quote/MITK/financials?p=MITK",
+        ]
+        '''
+        #print("urls = ", urls)
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+    def parse(self, response):
+        stockSymbol = response.url.split("=")[-1]
+        dataKey = "data-reactid"
+        items = response.css('span').extract()
+        print(items)
+        print("size of items = ", len(items))
+        filename = '%s-finance.txt' % stockSymbol
+        with open(filename, 'w+') as f:
+            for item in items:
+                result = self.extractInfo(item)
+                if result != "":
+                    f.write(result)
+                    f.write('\n')
+        self.log('Saved file %s' % filename)
+    def extractInfo(self, line):
+        if "data-reactid" not in line:
+            return ""
+        matchObj =  re.match(r'<span data-reactid=\"(\d+)\">(.*)</span>', line, re.M|re.I)
+        ret=""
+        if matchObj and matchObj.group(2) != "":
+            ret = matchObj.group(1)+"="+matchObj.group(2)
+        return ret
